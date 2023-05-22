76b197d66241d6917e4703847a73f23cd2114099
nick_chien <nick-tw@bobi.homes>
2023-05-22 09:42:59 +0800

    add transformService

--- src/main/java/com/datacenter/canal/transform/TransformService.java
+++ src/main/java/com/datacenter/canal/transform/TransformService.java
@@ -1,10 +1,15 @@
 package com.datacenter.canal.transform;
 
+import com.datacenter.canal.select.support.EtlColumn;
 import com.datacenter.canal.select.support.EtlMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -12,6 +17,26 @@ public class TransformService {
 
     public List<EtlMessage> transform(List<EtlMessage> messages) {
         log.debug("do transform");
+        Set<String> columnMaskedSet = new HashSet<>();
+        columnMaskedSet.add("password");
+        messages.stream()
+                .flatMap(message -> message.getData().stream())
+                .flatMap(map -> map.values().stream())
+                .map(column -> {
+                    if (columnMaskedSet.contains(column.getName())) {
+                        column.setValue(columnValMasked("*", column.getValue().toString().length()));
+                    }
+                    return column;
+                }).collect(Collectors.toList());
         return messages;
+    }
+
+    private static String columnValMasked(String str, int count){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            stringBuilder.append(str);
+        }
+
+        return stringBuilder.toString();
     }
 }
